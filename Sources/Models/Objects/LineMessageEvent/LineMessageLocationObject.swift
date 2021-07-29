1675dab11a3bb084b7dc7b5dc533edce42b4bdc2
Nier Liu <curse0122@gmail.com>
2021-07-29 17:08:45 +0800

    update location message object

--- Sources/Models/Objects/LineMessageEvent/LineMessageLocationObject.swift
+++ Sources/Models/Objects/LineMessageEvent/LineMessageLocationObject.swift
@@ -4,7 +4,7 @@ public struct LineMessageLocationObject: LineMessageEventObject, Codable {
     
     public var id: String
     public var type: LineMessageEventObjectType = .location
-    public var title: String
+    public var title: String?
     public var address: String
     public var latitude: Double
     public var longitude: Double
