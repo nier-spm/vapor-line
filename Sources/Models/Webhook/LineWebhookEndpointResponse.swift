c14f372f24d6b88cb1a8f1eed6c3006fd8373813
Nier Liu <curse0122@gmail.com>
2021-08-04 11:47:14 +0800

    add webhook api models document

--- Sources/Models/Webhook/LineWebhookEndpointResponse.swift
+++ Sources/Models/Webhook/LineWebhookEndpointResponse.swift
@@ -1,5 +1,11 @@
 import Vapor
 
+/**
+ - `endpoint`: Webhook URL.
+ - `active`: Webhook usage status. Send a webhook event from the LINE platform to the webhook URL only if enabled.
+    - `true`: Webhook usage is enabled.
+    - `false`: Webhook usage is disabled.
+ */
 public struct LineWebhookEndpointResponse: Content {
     
     public var endpoint: String
